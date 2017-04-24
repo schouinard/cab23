db4ead350340127e305ebded7b311969cf74a9c2
schouinard <stephane.chouinard@gmail.com>
2017-04-24 19:20:21 +0000

    Refactor: utilisation de Route::resource au lieu de tout nommer

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,11 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/', function () {
         return view('welcome');
     });
-    Route::get('/benevoles', 'BenevoleController@index');
-    Route::get('/benevoles/{benevole}', 'BenevoleController@show');
+    
+    Route::resource('benevoles', 'BenevoleController');
+    Route::resource('beneficiaires', 'BeneficiaireController');
+
     Route::post('/benevoles/{benevole}/services', 'ServiceController@store');
-    Route::post('/benevoles', 'BenevoleController@store');
-    Route::get('/beneficiaires', 'BeneficiaireController@index');
-    Route::get('/beneficiaires/{beneficiaire}', 'BeneficiaireController@show');
     Route::post('/beneficiaires/{beneficiaire}/services', 'ServiceController@store');
-    Route::post('/beneficiaires', 'BeneficiaireController@store');
 });
 
