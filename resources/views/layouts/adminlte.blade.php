b6e715c3b1236de9a2a6d9b35326c29e6d679b1e
schouinard <stephane.chouinard@gmail.com>
2017-04-26 17:06:15 +0000

    Activer les caches sur les datatables pour le saveState

--- resources/views/layouts/adminlte.blade.php
+++ resources/views/layouts/adminlte.blade.php
@@ -25,7 +25,8 @@
     $('.datatable').DataTable({
         language: {
             url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/French.json"
-        }
+        },
+        stateSave: true
     });
 
     $('.textarea').wysihtml5({
