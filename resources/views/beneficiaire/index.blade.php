f67f87d2182e3fd52baa7583116887339a741a11
schouinard <stephane.chouinard@gmail.com>
2017-04-26 17:11:44 +0000

    listes de personnes = id, nom, prenom, telephone 1, courriel, quartier

--- resources/views/beneficiaire/index.blade.php
+++ resources/views/beneficiaire/index.blade.php
@@ -14,17 +14,23 @@
                     <table class="datatable table table-hover table-bordered">
                         <thead>
                         <tr>
+                            <th>Id</th>
                             <th>Nom</th>
                             <th>Prenom</th>
                             <th>Courriel</th>
+                            <th>Telephone</th>
+                            <th>Quartier</th>
                         </tr>
                         </thead>
                         <tbody>
                         @foreach($beneficiaires as $beneficiaire)
                             <tr>
+                                <td>{{ $beneficiaire->id }}</td>
                                 <td><a href="{{ $beneficiaire->path() }}">{{ $beneficiaire->nom }}</a></td>
                                 <td>{{ $beneficiaire->prenom }}</td>
                                 <td>{{ $beneficiaire->email }}</td>
+                                <td>{{ $beneficiaire->telephone }}</td>
+                                <td>{{ $beneficiaire->quartier }}</td>
                             </tr>
                         @endforeach
                         </tbody>
--- resources/views/benevole/index.blade.php
+++ resources/views/benevole/index.blade.php
@@ -14,17 +14,23 @@
                     <table class="datatable table table-bordered table-hover">
                         <thead>
                         <tr>
+                            <th>Id</th>
                             <th>Nom</th>
                             <th>Prenom</th>
                             <th>Courriel</th>
+                            <th>Telephone</th>
+                            <th>Quartier</th>
                         </tr>
                         </thead>
                         <tbody>
                         @foreach($benevoles as $benevole)
                             <tr>
+                                <td>{{ $benevole->id }}</td>
                                 <td><a href="{{ $benevole->path() }}">{{ $benevole->nom }}</a></td>
                                 <td>{{ $benevole->prenom }}</td>
                                 <td>{{ $benevole->email }}</td>
+                                <td>{{ $benevole->telephone }}</td>
+                                <td>{{ $benevole->quartier }}</td>
                             </tr>
                         @endforeach
                         </tbody>
